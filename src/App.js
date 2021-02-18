86e8ef7567968693dda597bfb9b56943fe04555f
Damian Janus <damian.janus@gmail.com>
2021-02-18 18:12:32 +0100

    circle animation fade, expand and shrink

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { TweenMax, Power3 } from 'gsap';
+import { useRef, useEffect, useState } from 'react';
+
 function App() {
+  const [state, setState] = useState(false);
+  const app = useRef(null);
+  const circlePink = useRef(null);
+  const circleOrange = useRef(null);
+  const circleCoral = useRef(null);
+
+  useEffect(() => {
+    TweenMax.to(app.current, 0, { css: { visibility: 'visible'}})
+    // TweenMax.from(circlePink.current, .8, { opacity: 0, x: 50, ease: Power3.easeOut})
+    // TweenMax.from(circleOrange.current, .8, { opacity: 0, x: 50, ease: Power3.easeOut, delay: .2})
+    // TweenMax.from(circleCoral.current, .8, { opacity: 0, x: 50, ease: Power3.easeOut, delay: .4})
+    TweenMax.staggerFrom([circlePink.current, circleOrange.current, circleCoral.current], .8, { opacity: 0, x: 50, ease: Power3.easeOut}, .2)
+  }, [])
+
+  const handleExpand = (e) => {
+    TweenMax.to(e.target, .8, { width: 200, height: 200, ease: Power3.easeOut});
+    setState(true);
+  }
+
+  const handleShrink = (e) => {
+    TweenMax.to(e.target, .8, { width: 100, height: 100, ease: Power3.easeOut});
+    setState(false)
+  }
+
   return (
-    <div className="App">
+    <div className="App" ref={app}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="circle-container">
+          <div className="circle pink" ref={circlePink} onClick={state ? handleShrink : handleExpand}></div>
+          <div className="circle orange" ref={circleOrange} onClick={state ? handleShrink : handleExpand}></div>
+          <div className="circle coral" ref={circleCoral} onClick={state ? handleShrink : handleExpand}></div>
+        </div>
       </header>
     </div>
   );
